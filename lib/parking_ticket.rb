2e346e453f04ed317085654d6cfb1e77abeaaf61
Tom Ecrepont <tomecrepont@gmail.com>
2023-01-13 08:25:08 +0100

    Update ParkingTicket into ParkingTicket::Base that can now be instanciated with the required configuration

--- lib/parking_ticket.rb
+++ lib/parking_ticket.rb
@@ -2,44 +2,71 @@
 
 require 'parking_ticket/configuration'
 require 'parking_ticket/version'
-require 'client/pay_by_phone'
+
+# PayByPhone wrapper
+require 'client/pay_by_phone/configuration'
 require 'client/pay_by_phone/adapter'
+require 'client/pay_by_phone/request'
 
 module ParkingTicket
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
+  class Base
+    attr_reader :configuration
 
-  def self.reset
-    @configuration = Configuration.new
-  end
-
-  def self.configure
-    yield(configuration)
-  end
-
-  def self.configured?
-    self&.adapter&.ready?
-  end
-
-  class Error < StandardError; end
-
-  def self.renew
-    if current_ticket
-      puts '❌ Can not renew ticket as already covered by a ticket at this time'
-    else
-      puts '🔄 Renewing ticket'
-      adapter.renew
-      puts '✅ Ticket renewed'
+    class Error < StandardError
     end
-  end
 
-  def self.current_ticket
-    puts '🕵️ Retrieving current_ticket'
-    adapter.current_ticket
-  end
+    def initialize(adapter_name, configuration_attributes)
+      @adapter_name = adapter_name
+      @configuration_attributes = configuration_attributes
+    end
 
-  def self.adapter
-    configuration.adapter
+    def adapter
+      @adapter ||= load_adapter!
+    end
+
+    def renew
+      if current_ticket
+        puts '❌ Can not renew ticket as already covered by a ticket at this time'
+      else
+        puts '🔄 Renewing ticket'
+        adapter.renew
+        puts '✅ Ticket renewed'
+      end
+    end
+
+    def current_ticket
+      puts '🕵️ Retrieving current_ticket'
+      adapter.current_ticket
+    end
+
+    private
+
+    def load_adapter!
+      return prepare_pay_by_phone_adapter! if @adapter_name == 'pay_by_phone'
+      return prepare_easy_park_adapter! if @adapter_name == 'easy_park'
+
+      raise Error, "Unhandled adapter : #{@adapter_name}"
+    end
+
+    def prepare_pay_by_phone_adapter!
+      configuration = pay_by_phone_configuration
+      return Client::PayByPhone::Adapter.new(configuration) if configuration.completed?
+
+      raise Error, 'Uncompleted configuration'
+    end
+
+    def pay_by_phone_configuration
+      Client::PayByPhone::Configuration.new do |config|
+        config.username = @configuration_attributes[:username]
+        config.password = @configuration_attributes[:password]
+        config.license_plate = @configuration_attributes[:license_plate]
+        config.zipcode = @configuration_attributes[:zipcode]
+        config.card_number = @configuration_attributes[:card_number]
+      end
+    end
+
+    def prepare_easy_park_adapter!
+      raise Error, 'EasyPark will be handled in the next major release'
+    end
   end
 end
--- spec/parking_ticket_spec.rb
+++ spec/parking_ticket_spec.rb
@@ -1,11 +1,144 @@
 # frozen_string_literal: true
 
-RSpec.describe ParkingTicket do
-  it "has a version number" do
-    expect(ParkingTicket::VERSION).not_to be nil
-  end
+module ParkingTicket
+  RSpec.describe Base do
+    subject do
+      described_class.new(
+        adapter_name,
+        configuration_attributes
+      )
+    end
+    let(:adapter_name) { nil }
+    let(:configuration_attributes) { nil }
+    it 'has a version number' do
+      expect(ParkingTicket::VERSION).not_to be nil
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    describe '#instance_methods' do
+      describe '#current_ticket' do
+        context 'when it is pay_by_phone' do
+          let(:adapter_name) { 'pay_by_phone' }
+          let(:configuration_attributes) do
+            {
+              username: ENV['PARKING_TICKET_USERNAME'],
+              password: ENV['PARKING_TICKET_PASSWORD'],
+              license_plate: ENV['PARKING_TICKET_LICENSEPLATE'],
+              zipcode: ENV['PARKING_TICKET_ZIPCODE'],
+              card_number: ENV['PARKING_TICKET_CARDNUMBER']
+            }
+          end
+          it 'uses the pay_by_phone adapter' do
+            pay_by_phone_adapter_double = instance_double(ParkingTicket::Client::PayByPhone::Adapter)
+            allow(ParkingTicket::Client::PayByPhone::Adapter).to receive(:new).and_return(pay_by_phone_adapter_double)
+            expect(pay_by_phone_adapter_double).to receive(:current_ticket)
+            subject.current_ticket
+          end
+
+          describe 'errors' do
+            context 'when the configuration is not complete' do
+              let(:configuration_attributes) do
+                {
+                  username: ENV['PARKING_TICKET_USERNAME'],
+                  password: ENV['PARKING_TICKET_PASSWORD'],
+                  license_plate: ENV['PARKING_TICKET_LICENSEPLATE'],
+                  zipcode: ENV['PARKING_TICKET_ZIPCODE']
+                }
+              end
+              it 'raises a Uncompleted configuration error' do
+                expect do
+                  subject.current_ticket
+                end.to raise_error(ParkingTicket::Base::Error, 'Uncompleted configuration')
+              end
+            end
+          end
+        end
+        context 'when the adapter is easy_park' do
+          let(:adapter_name) { 'easy_park' }
+          it 'raises a EasyPark will be handled in the next major release error' do
+            expect do
+              subject.current_ticket
+            end.to raise_error(ParkingTicket::Base::Error, 'EasyPark will be handled in the next major release')
+          end
+        end
+        context 'when the adapter is something else' do
+          let(:adapter_name) { 'something_else' }
+          it 'raises a Unhandled adapter error' do
+            expect do
+              subject.current_ticket
+            end.to raise_error(ParkingTicket::Base::Error, 'Unhandled adapter : something_else')
+          end
+        end
+      end
+      describe '#renew' do
+        context 'when it is pay_by_phone' do
+          let(:adapter_name) { 'pay_by_phone' }
+          let(:configuration_attributes) do
+            {
+              username: ENV['PARKING_TICKET_USERNAME'],
+              password: ENV['PARKING_TICKET_PASSWORD'],
+              license_plate: ENV['PARKING_TICKET_LICENSEPLATE'],
+              zipcode: ENV['PARKING_TICKET_ZIPCODE'],
+              card_number: ENV['PARKING_TICKET_CARDNUMBER']
+            }
+          end
+          context 'when there is no current_ticket' do
+            it 'uses the pay_by_phone adapter to renew a ticket' do
+              pay_by_phone_adapter_double = instance_double(
+                ParkingTicket::Client::PayByPhone::Adapter,
+                current_ticket: nil
+              )
+              allow(ParkingTicket::Client::PayByPhone::Adapter).to receive(:new).and_return(pay_by_phone_adapter_double)
+              expect(pay_by_phone_adapter_double).to receive(:renew)
+              subject.renew
+            end
+          end
+          context 'when there is a current_ticket' do
+            it 'uses the pay_by_phone adapter to renew a ticket' do
+              pay_by_phone_adapter_double = instance_double(
+                ParkingTicket::Client::PayByPhone::Adapter,
+                current_ticket: {}
+              )
+              allow(ParkingTicket::Client::PayByPhone::Adapter).to receive(:new).and_return(pay_by_phone_adapter_double)
+              expect(pay_by_phone_adapter_double).not_to receive(:renew)
+              subject.renew
+            end
+          end
+
+          describe 'errors' do
+            context 'when the configuration is not complete' do
+              let(:configuration_attributes) do
+                {
+                  username: ENV['PARKING_TICKET_USERNAME'],
+                  password: ENV['PARKING_TICKET_PASSWORD'],
+                  license_plate: ENV['PARKING_TICKET_LICENSEPLATE'],
+                  zipcode: ENV['PARKING_TICKET_ZIPCODE']
+                }
+              end
+              it 'raises a Uncompleted configuration error' do
+                expect do
+                  subject.renew
+                end.to raise_error(ParkingTicket::Base::Error, 'Uncompleted configuration')
+              end
+            end
+          end
+        end
+        context 'when the adapter is easy_park' do
+          let(:adapter_name) { 'easy_park' }
+          it 'raises a EasyPark will be handled in the next major release error' do
+            expect do
+              subject.current_ticket
+            end.to raise_error(ParkingTicket::Base::Error, 'EasyPark will be handled in the next major release')
+          end
+        end
+        context 'when the adapter is something else' do
+          let(:adapter_name) { 'something_else' }
+          it 'raises a Unhandled adapter error' do
+            expect do
+              subject.current_ticket
+            end.to raise_error(ParkingTicket::Base::Error, 'Unhandled adapter : something_else')
+          end
+        end
+      end
+    end
   end
 end
